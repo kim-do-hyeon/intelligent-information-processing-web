2701cb4690b852ead46f8d33a4620d97e773de0e
pental <pental@kakao.com>
2022-09-14 00:55:00 +0900

    Redirect index page

--- apps/authentication/routes.py
+++ apps/authentication/routes.py
@@ -17,6 +17,7 @@ from apps.authentication.util import verify_pass
 
 @blueprint.route('/')
 def route_default():
+    return redirect(url_for('home_blueprint.index'))
     return redirect(url_for('authentication_blueprint.login'))
 
 
--- run.py
+++ run.py
@@ -1,8 +1,4 @@
 # -*- encoding: utf-8 -*-
-"""
-Copyright (c) 2019 - present AppSeed.us
-"""
-
 import os
 from   flask_migrate import Migrate
 from   flask_minify  import Minify
