0b0aa6a047a53cabeb8bcb166d686d14f1b1e559
pental <pental@kakao.com>
2022-09-13 22:46:37 +0900

    Using templates

---
+++ apps/authentication/models.py
@@ -0,0 +1,40 @@
+# -*- encoding: utf-8 -*-
+from flask_login import UserMixin
+
+from apps import db, login_manager
+
+from apps.authentication.util import hash_pass
+
+class Users(db.Model, UserMixin):
+
+    __tablename__ = 'Users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True)
+    email = db.Column(db.String(64), unique=True)
+    password = db.Column(db.LargeBinary)
+
+    def __init__(self, **kwargs):
+        for property, value in kwargs.items():
+            if hasattr(value, '__iter__') and not isinstance(value, str):
+                value = value[0]
+
+            if property == 'password':
+                value = hash_pass(value)
+
+            setattr(self, property, value)
+
+    def __repr__(self):
+        return str(self.username)
+
+
+@login_manager.user_loader
+def user_loader(id):
+    return Users.query.filter_by(id=id).first()
+
+
+@login_manager.request_loader
+def request_loader(request):
+    username = request.form.get('username')
+    user = Users.query.filter_by(username=username).first()
+    return user if user else None
